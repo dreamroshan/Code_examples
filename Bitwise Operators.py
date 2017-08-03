b26a8dea8407c278e48395e20f86e0b9e051f89e
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-03 16:39:28 +0300

    Add files via upload

---
+++ Bitwise Operators.py
@@ -0,0 +1,20 @@
+a = 60;
+b = 13;
+
+c = a & b;
+print "Value of c:", c;
+
+c = a | b;
+print"Value of c: ",c;
+
+c = a^b;
+print "Value of c :",c;
+
+c = ~a;
+print "Value of c: ",c;
+
+c = a << 2;
+print "Value of c:",c;
+
+c = a >> 2;
+print "Value of c:",c;
