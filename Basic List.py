ee1f883fa223703bf15a4ec093f652ff225b4e7b
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-03 16:25:00 +0300

    Add files via upload

---
+++ Basic List.py
@@ -0,0 +1,17 @@
+'''
+@ author : CreativeCub
+'''
+
+list1 = ['SuperMan','BatMan','The Mask'];
+
+#Print entire list
+print list1
+
+#Print with index value
+print list1[0]
+print list1[1]
+print list1[2]
+
+#Delete list element
+del list1[0]
+print list1
