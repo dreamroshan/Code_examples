9079352b5d097fc5edc6c3484c24f04abc7b24a5
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-03 12:45:35 +0300

    Add files via upload

---
+++ Array Example.py
@@ -0,0 +1,15 @@
+#Creating an array
+array = []
+
+#Inserting values in the Array
+array.append(1)
+array.append(3)
+array.append(5)
+
+print("Elements of an Array : "+str(array))
+
+#Changing the value of array[0]
+array[0] = 2
+print("Elements of an Array after changes:" + str(array))
+
+print ("First Element of an Array : " +str(array[0]))
