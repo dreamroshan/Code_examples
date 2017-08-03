7dadcf1ccccab9ca67d21c410351bf98d7f2d411
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-03 03:47:14 +0300

    Add files via upload

---
+++ Arithmetic Operator.py
@@ -0,0 +1,27 @@
+a = 20;
+b = 10;
+
+c = a + b;
+print "Addition Value: ",c;
+
+c = a - b;
+print "Subtraction: ",c;
+
+c = a * b;
+print "Multiplication Value: ",c;
+
+c = a / b;
+print "Division Value: ", c;
+
+c = a % b;
+print "Mod Value: ",c;
+
+a = 2;
+b = 3;
+c = a**b;
+print "Exponent Value: ",c;
+
+a = 9;
+b = 4;
+c = a//b;
+print "Floor Division Value:",c;
