8f94141e6f8274663a99516d15064ce3df330bf4
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-03 17:22:24 +0300

    Add files via upload

---
+++ Celsius to Farenheit.py
@@ -0,0 +1,9 @@
+# take input from the user in degree celsius
+celsius = float(input('Enter degree Celsius:'))
+
+#calculate fahrenheit
+
+fahrenheit = (celsius * 1.8) + 32
+
+print
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit'%(celsius,fahrenheit))
