4d71064f37f038b5046b71233be1b39482b8f03a
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-03 03:41:03 +0300

    Add files via upload

---
+++ Area of Triangle using Heron's Formula.py
@@ -0,0 +1,12 @@
+# Three sides of the triangle a,b,c are provided by the user
+
+a = float(input('Enter first side: '))
+b = float(input('Enter second side:'))
+c = float(input('Enter third side:'))
+
+# calculate the semi-perimeter
+s = (a + b + c) / 2
+
+#calculate the area
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle is %0.2f' %area)
