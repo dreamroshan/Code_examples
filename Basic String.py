e33b1af24da68a3354cca7fbd2138fa9a0274b79
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-03 16:28:35 +0300

    Add files via upload

---
+++ Basic String.py
@@ -0,0 +1,13 @@
+'''
+@author: CreativeCub
+'''
+
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+#Print entire String
+print var1
+
+#Print SubString from String
+print "var1[0]: ",var1[0]
+print "var2[1:6]",var2[1:6]
