efd6db1cf223136b6f84001e05de7a89e28eb458
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-03 03:25:45 +0300

    Add files via upload

---
+++ Area of Square.py
@@ -0,0 +1,7 @@
+
+#Area of a square is side * side where side is sides of square.
+def areaofsquare(side):
+    return side * side;
+
+side = int(raw_input("Enter value of the sides of square : "))
+print "Area of square : ",areaofsquare(side)
