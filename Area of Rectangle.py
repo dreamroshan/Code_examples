a8dfd50ac6f2cdff4200c26f11319a487572699c
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-03 03:16:53 +0300

    Add files via upload

---
+++ Area of Rectangle.py
@@ -0,0 +1,8 @@
+#Area of a rectangle = length * width.
+
+def areaofrectangle(l,w):
+    return l*w;
+length = int(raw_input("Enter the length of Rectangle : "))
+width =  int(raw_input("Enter the width of Rectangle :"))
+
+print "Area pf rectangle : ", areaofrectangle(length,width)
