500c1a4822fe8a0fdea2b0cd2bb22fec4622ca17
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-05 01:55:37 +0300

    Add files via upload

---
+++ For Each Loop.py
@@ -0,0 +1,13 @@
+d = {3: "Earth",1: "Mercury",4: "Mars",2: "Venus"}
+
+#for each loop
+for k in sorted(d):
+    print("%i: %s" % (k,d[k]))
+print
+
+d = {"London":"United Kingdom","Berlin": "Germany","Rome": "Italy","Paris": "France"}
+
+#for each loop
+
+for k in sorted(d):
+    print("%s: %s" % (k,d[k]))
