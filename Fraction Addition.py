c36f54a3a43e6cde592fef169e379ec86354b8bc
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-05 02:01:53 +0300

    Add files via upload

---
+++ Fraction Addition.py
@@ -0,0 +1,11 @@
+from fractions import*
+
+a = Fraction(raw_input("Enter first fraction number:"))
+
+print
+
+b = Fraction(raw_input("Enter second fraction number:"))
+
+print
+
+print "Addition of",a,"and",b,"is :",(a+b)
