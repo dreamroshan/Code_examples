97f7be494bc9aa00d444330da0a06c5e74cbd983
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-04 19:38:29 +0300

    Add files via upload

---
+++ Escape Characters.py
@@ -0,0 +1,20 @@
+'''
+@author : CreativeCub
+'''
+
+var1 = 'Hello World!'
+var2 = "Python Programming"
+
+#Print entire String
+print var1
+
+#Escape character \t is used for Tab
+print 'This is \t',var1
+print 'Thisis \t\t',var2
+
+#Print for indentation or empty line
+print ''
+
+#Escape character \n is used for Newline
+print 'This is \n',var1
+print 'This is \n\n',var2
