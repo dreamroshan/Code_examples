0aac908ef0b5688ede39ed33621e311e9d79ba85
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-04 18:58:42 +0300

    Add files via upload

---
+++ Diamond Pattern.py
@@ -0,0 +1,6 @@
+side = int(input("Enter the length of the diamond pattern : "))
+
+print
+
+for x in list(range(side)) + list(reversed(range(side-1))):
+    print('{: <{w1}}{:*<{w2}}'.format('','',w1 = side-x-1,w2 = x*2+1))
