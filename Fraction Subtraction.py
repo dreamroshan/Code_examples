8495c88108ea8d387387dbde4ff2ab074cc19871
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-05 02:05:18 +0300

    Add files via upload

---
+++ Fraction Subtraction.py
@@ -0,0 +1,10 @@
+from fractions import*
+
+a = Fraction(raw_input("Enter first fraction number :"))
+
+print
+
+b = Fraction(raw_input("Enter second fraction number :"))
+
+print
+print "Subtraction of",a,"and",b,"is :",(a-b)
