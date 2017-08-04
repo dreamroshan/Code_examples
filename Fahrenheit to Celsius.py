f0607c22fbf1032659812edf68f67c6fadf26a99
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-04 23:54:19 +0300

    Add files via upload

---
+++ Fahrenheit to Celsius.py
@@ -0,0 +1,8 @@
+#take user input
+F = float(raw_input('Enter temperature in Fahrenheit :'))
+
+#conver fahrenheit to celsius
+C = ((5.0/9.0)*(F-32))
+
+print
+print "Temperature in degree Celsius is %f" %C
