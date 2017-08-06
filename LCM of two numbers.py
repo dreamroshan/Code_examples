838ffde0cd79f35b76e3022942767288e0b46e04
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-06 16:43:19 +0300

    Add files via upload

---
+++ LCM of two numbers.py
@@ -0,0 +1,22 @@
+# define a function
+def lcm(x,y):
+    """ This function takes two intefers and returns the L.C.M."""
+
+    #choose the greater number
+    if x > y:
+        greater = x
+    else:
+        greater = y
+
+    while(True):
+        if((greater %x == 0) and (greater % y == 0)):
+            lcm = greater
+            break
+        greater += 1
+    return lcm
+
+# take input from the user
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number:"))
+
+print "The L:C.M. of",num1,"and",num2,"is",lcm(num1,num2)
