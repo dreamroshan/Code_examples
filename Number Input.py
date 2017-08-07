b213312f8cbd60d8986a976ecbf4d5d88fa4ad02
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-07 14:47:42 +0300

    Add files via upload

---
+++ Number Input.py
@@ -0,0 +1,11 @@
+'''
+@author :CreativeCub
+'''
+
+#using raw_input
+age = raw_input('Enter your age:')
+print("Your age is %d"%int(age) );
+
+#using input
+number = input('\nEnter any number:')
+print("You entered %d" %number);
