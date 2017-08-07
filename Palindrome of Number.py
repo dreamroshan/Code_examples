47dddd812f8c3f38ebfd5c877bbde93e8a816ccc
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-07 15:25:55 +0300

    Add files via upload

---
+++ Palindrome of Number.py
@@ -0,0 +1,15 @@
+def Palindrome_Number(n):
+
+    m=n
+    a = 0
+    while(m!=0):
+        a = m % 10 + a* 10
+        m = m / 10
+
+    if(n == a):
+        print('%d is a palindrome number'%n)
+    else:
+        print('%d is not a palindrome number'%n)
+
+n = input('Enter Number to check for palindromee ')
+Palindrome_Number(n)
