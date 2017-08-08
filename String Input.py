556f9ce6ac95c8673ad37264869bc0bcb533c30f
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-08 04:25:48 +0300

    Add files via upload

---
+++ String Input.py
@@ -0,0 +1,13 @@
+'''
+
+@author : CreativeCub
+'''
+
+#using raw_input
+name=raw_input('Enter your name :')
+print("Hi %s, Let us be friends!"%name);
+
+#using only input
+#remember to you double quotes(" ")while giving input
+place=input('\nWhere do you live? : ')
+print("I live in %s" %place);
