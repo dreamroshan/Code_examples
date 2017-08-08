74c2005a8941ef74be9936e7de7f05688f600f8b
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-08 04:19:27 +0300

    Add files via upload

---
+++ String Concantenation.py
@@ -0,0 +1,12 @@
+'''
+
+@author :CreativeCub
+'''
+
+#String Concat Example
+var1 = 'hello Harry'
+var2 =var1 + 'Potter!!'
+print var2
+
+#String Replace 'Harry Potter' with 'Muggle'
+print var2[:6] + 'Muggle'
