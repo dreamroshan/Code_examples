cfd85c2238866ca2ec31f1fb5f45e9ac6d9576c3
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-08 05:07:40 +0300

    Add files via upload

---
+++ String Methods Basic.py
@@ -0,0 +1,21 @@
+'''
+@author : CreativeCub
+'''
+
+var1 = "python"
+var2 = "Programming"
+
+#Capitalizing first letter of string
+print 'The Capitalized String is: ',var1.capitalize()
+
+#Getting the length of the string
+print 'The length of the String is:',len(var1)
+
+#Converts uppercase letters in string to lowercase
+print 'The String in lowercase :',var1.lower()
+
+#Converts lowercase letters in string to uppercase
+print 'The String in uppercase: ',var1.upper()
+
+#Converts lowercase letters to uppercase and viceversa
+print 'The String in inverted case:',var2.swapcase()
