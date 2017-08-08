d8e7e30c7a9ac74b42d7309f980fbce613f896f8
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-08 05:59:19 +0300

    Add files via upload

---
+++ Volumen of a Cylinder.py
@@ -0,0 +1,8 @@
+#Volume of Circle Example
+import math
+radius = float(raw_input("Enter the radius of the cylinder in cm:"))
+height = float(raw_input("Enter the height of the cylinder in cm:"))
+
+volume = math.pi*radius*height
+
+print("Volume of the Cylinder is",volume,"cubic cm.")
