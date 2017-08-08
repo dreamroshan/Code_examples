400ad6bd49f2e9b70193f220f7bf1410e2741397
Roshan Gupta <30659041+dreamroshan@users.noreply.github.com>
2017-08-08 05:51:51 +0300

    Add files via upload

---
+++ User Input.py
@@ -0,0 +1,7 @@
+num1 = int(raw_input("\nEnter integer value: "));
+num2 = float(raw_input("\nEnter float value: "));
+str = raw_input("\nEnter String: ");
+
+print "\nInteger Value Entered :",num1;
+print "\nFloat Value Entered: ",num2;
+print"\nString Value:",str;
